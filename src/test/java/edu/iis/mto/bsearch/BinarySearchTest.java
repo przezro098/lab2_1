8bbe28190860167cb0a07d3433d75051a2f2d173
Tomasz Kucharczyk <215786@edu.p.lodz.pl>
2020-04-01 16:30:25 +0200

    testy_v1 Searching for first element in multi sequence

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class BinarySearchTest {
 
     int[] singleElementSeq = {0};
+    int[] MultiElementSeq = {0,1,2,3,4};
     BinarySearch binarySearch = BinarySearch.create();
 
     @Test
@@ -25,6 +26,15 @@ class BinarySearchTest {
         int pos = -1;
         var result = binarySearch.search(value, singleElementSeq);
         Assertions.assertFalse(result.isFound());
+        Assertions.assertEquals(result.getPosition(), pos);
+    }
+
+    @Test
+    void SearchingFirstElementInMultiSeqTest() {
+        int value = 0;
+        int pos = 0;
+        var result = binarySearch.search(value, MultiElementSeq);
+        Assertions.assertTrue(result.isFound());
         Assertions.assertEquals(result.getPosition(), pos);
     }
 
