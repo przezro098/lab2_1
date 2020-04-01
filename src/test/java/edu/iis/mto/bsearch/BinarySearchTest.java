651ed4846e5d295b831ee9f15fb56472e041569a
Tomasz Kucharczyk <215786@edu.p.lodz.pl>
2020-04-01 19:59:19 +0200

    testy_v2 Searching in empty sequence & BinarySearch throws IllegalArgumentException

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -1,12 +1,14 @@
 package edu.iis.mto.bsearch;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
+
 
 class BinarySearchTest {
 
+    int[] emptySeq = {};
     int[] singleElementSeq = {0};
     int[] MultiElementSeq = {0,1,2,3,4};
     int pos = 0;
@@ -65,6 +67,11 @@ class BinarySearchTest {
         var result = binarySearch.search(value, MultiElementSeq);
         assertThat(result.isFound(), is(false));
         assertThat(result.getPosition(), is(pos));
+    }
+
+    @Test
+    void SearchingInEmptySeq() {
+        assertThrows(IllegalArgumentException.class, () -> binarySearch.search(0, emptySeq));
     }
 
 
