f9d20347cdacab8a5d6be96eb6a38a5bdb134037
Tomasz Kucharczyk <215786@edu.p.lodz.pl>
2020-04-01 20:14:28 +0200

    testy_v2 removed unnecessary variables & namefix

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -10,63 +10,49 @@ class BinarySearchTest {
 
     int[] emptySeq = {};
     int[] singleElementSeq = {0};
-    int[] MultiElementSeq = {0,1,2,3,4};
-    int pos = 0;
-    int value = 0;
+    int[] multiElementSeq = {0,1,2,3,4};
     BinarySearch binarySearch = BinarySearch.create();
 
     @Test
     void SearchingExistingElementInSingleSeq() {
-        value = 0;
-        pos = 0;
-        var result = binarySearch.search(value, singleElementSeq);
+        var result = binarySearch.search(0, singleElementSeq);
         assertThat(result.isFound(), is(true));
-        assertThat(result.getPosition(), is(pos));
+        assertThat(result.getPosition(), is(0));
     }
 
     @Test
     void SearchingNoneExistingElementInSingleSeq() {
-        value = 1;
-        pos = -1;
-        var result = binarySearch.search(value, singleElementSeq);
+        var result = binarySearch.search(1, singleElementSeq);
         assertThat(result.isFound(), is(false));
-        assertThat(result.getPosition(), is(pos));
+        assertThat(result.getPosition(), is(-1));
     }
 
     @Test
     void SearchingFirstElementInMultiSeq() {
-        value = 0;
-        pos = 0;
-        var result = binarySearch.search(value, MultiElementSeq);
+        var result = binarySearch.search(0, multiElementSeq);
         assertThat(result.isFound(), is(true));
-        assertThat(result.getPosition(), is(pos));
+        assertThat(result.getPosition(), is(0));
     }
 
     @Test
     void SearchingLastElementInMultiSeq() {
-        value = 4;
-        pos = 4;
-        var result = binarySearch.search(value, MultiElementSeq);
+        var result = binarySearch.search(4, multiElementSeq);
         assertThat(result.isFound(), is(true));
-        assertThat(result.getPosition(), is(pos));
+        assertThat(result.getPosition(), is(4));
     }
 
     @Test
     void SearchingCentralElementInMultiSeq() {
-        value = 2;
-        pos = 2;
-        var result = binarySearch.search(value, MultiElementSeq);
+        var result = binarySearch.search(2, multiElementSeq);
         assertThat(result.isFound(), is(true));
-        assertThat(result.getPosition(), is(pos));
+        assertThat(result.getPosition(), is(2));
     }
 
     @Test
     void SearchingNoneExistingElementInMultiSeq() {
-        value = 5;
-        pos = -1;
-        var result = binarySearch.search(value, MultiElementSeq);
+        var result = binarySearch.search(5, multiElementSeq);
         assertThat(result.isFound(), is(false));
-        assertThat(result.getPosition(), is(pos));
+        assertThat(result.getPosition(), is(-1));
     }
 
     @Test
